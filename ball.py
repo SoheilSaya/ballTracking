ae9ae3e1c1cc67b5e208f27148e3e456330f04aa
SoheilSaya <soheil.fa201@gmail.com>
2023-07-19 23:21:09 +0330

    just testing

--- ball.py
+++ ball.py
@@ -8,7 +8,7 @@ import argparse
 import cv2
 import imutils
 import time
-
+print("hello github")
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-v", "--video",
