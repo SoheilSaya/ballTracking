290813e2707e8e8f8d3bc47c1de89a75fdba72a0
SoheilSaya <soheil.fa201@gmail.com>
2023-07-20 12:46:48 +0330

    Grids, resolution and areas.
    Changeable grids are added.
    resolution can now be changed manually.
    Also we get output for which grid area the object is in.

--- ball.py
+++ ball.py
@@ -1,6 +1,4 @@
-# USAGE
-# python ball_tracking.py
-# import the necessary packages
+
 from collections import deque
 from imutils.video import VideoStream
 import numpy as np
@@ -8,7 +6,7 @@ import argparse
 import cv2
 import imutils
 import time
-print("hello github")
+
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-v", "--video",
@@ -32,12 +30,25 @@ if not args.get("video", False):
 # otherwise, grab a reference to the video file
 else:
 	vs = cv2.VideoCapture(args["video"])
+frame = vs.read()
 
-# allow the camera or video file to warm up
-time.sleep(2.0)
+#constants define
+fragments_number=16
+frame_height = 960
+frame_width = 1280
+
+
+#random numbers to look cool
+print(frame_height,frame_width)
+print(frame.shape[0],frame.shape[1])
+
+
+# allow the camera or video file to warm up 
+#time.sleep(2.0)
 
 # keep looping
 while True:
+	
 	# grab the current frame
 	frame = vs.read()
 
@@ -51,10 +62,16 @@ while True:
 
 	# resize the frame, blur it, and convert it to the HSV
 	# color space
-	frame = imutils.resize(frame, width=600)
+	
+	#resizing everything
+	frame =cv2.resize(frame, (frame_width, frame_height))
+	cv2.namedWindow("Frame")
+	cv2.resizeWindow("Frame", frame_width, frame_height)
 	blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+	#frame = imutils.resize(frame, width=640)
 
 	
+
 	hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
 
 	# construct a mask for the color "green", then perform
@@ -103,7 +120,44 @@ while True:
 		thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
 		cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
 		print(pts[i])
+		#time.sleep(0.)
+  
+		#deciding if object is in which area
+		print(int((pts[i][0])/(frame_width/fragments_number))+1,'x')
+		print(int(pts[i][1]/(frame_height/fragments_number))+1,'y')
+	
+ 
+ 
+ 
+ 
+	#DRAWING GRIDS
+	height, width = frame_height,frame_width
+	fragment_width = frame_width // fragments_number
+	fragment_height = frame_height // fragments_number
+
+	# Draw horizontal lines to divide the window into fragments
+	for r in range(1, fragments_number):
+		y = r * fragment_height
+		cv2.line(frame, (0, y), (frame_width, y), (255, 0, 0), 1)
+
+	# Draw vertical lines to divide the window into fragments
+	for c in range(1, fragments_number):
+		x = c * fragment_width
+		cv2.line(frame, (x, 0), (x, frame_height), (255, 0, 0), 1)
+  
+  
+  
+  
 	# show the frame to our screen
+	cv2.namedWindow("Frame")
+	# save a frame to an image file
+	#cv2.imwrite("frame.png", frame)
+
+	
+	#debug numbers to look even cooler
+	x, y, w, h = cv2.getWindowImageRect("Frame")
+	print(f"Window size: {w}x{h}")
+	print(f"Frame size: {frame_width}x{frame_height}")
 	cv2.imshow("Frame", frame)
 	key = cv2.waitKey(1) & 0xFF
 
